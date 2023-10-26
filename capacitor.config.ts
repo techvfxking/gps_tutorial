a12f83483ed1b903903811badb681a8f2d4ea9fc
Biplab Sharma <jobs.biplab.sharma@gmail.com>
2023-10-26 16:32:23 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gps_tutorial',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
